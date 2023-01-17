a5c9d073712207b4f160939ca7d9b1744bc4e487
Zack Lemur-Pro <rztaylor5@gmail.com>
2023-01-16 22:43:20 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
-from setuptools import setup, find_packages
-from distutils.sysconfig import get_python_lib
+from distutils.core import setup
 
 setup(
     name='CheUnitOp',
     version='0.0.1',
-    packages=find_packages(),
     description='Python library for simulating chemical engineering unit operations',
     author='Zack Taylor',
     platforms=["Linux", "Mac OS-X"],
-    #build_requires=['numpy>=1.8.0', 'setuptools'],
     install_requires=['numpy', 'scipy', 'matplotlib'],
-    package_data={'': ['*.txt']},
+    packages=['CheUnitOp'],
     license='Apache License, version 2.0',
     author_email='rztaylor5@gmail.com'
 )
